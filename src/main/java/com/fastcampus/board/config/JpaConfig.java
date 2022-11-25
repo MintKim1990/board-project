181b860b23814b2593a72caeb063ee9a98ffc7b7
MintKim1990 <rlaalsxo1990@gmail.com>
2022-11-25 22:40:57 +0900

    #10 - Spring Data Jpa 속성 설정
    * Auditing 기능 사용목적 Config 추가
    * DB 접근을 위한 프로퍼티 설정

---
+++ src/main/java/com/fastcampus/board/config/JpaConfig.java
@@ -0,0 +1,19 @@
+package com.fastcampus.board.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return () -> Optional.of("mint"); // TODO : 시큐리티 연동 시 변경 필요
+    }
+
+}
