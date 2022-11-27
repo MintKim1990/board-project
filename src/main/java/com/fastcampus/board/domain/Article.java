cf95b9aa465f23c5deab67d083f210a327b43f17
MintKim1990 <rlaalsxo1990@gmail.com>
2022-11-27 15:17:26 +0900

    #12 - AuditingFields 추출
    생성자, 생성일시, 수정자, 수정일시를 추출

--- src/main/java/com/fastcampus/board/domain/Article.java
+++ src/main/java/com/fastcampus/board/domain/Article.java
@@ -22,9 +22,8 @@ import java.util.Set;
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy")
 })
-@EntityListeners(AuditingEntityListener.class)
 @Entity
-public class Article {
+public class Article extends AuditingFields {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -46,22 +45,6 @@ public class Article {
     @OrderBy("id")
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "article", cascade = CascadeType.ALL)
     private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
-
-    @CreatedDate
-    @Column(nullable = false)
-    private LocalDateTime createdAt;
-
-    @CreatedBy
-    @Column(nullable = false, length = 100)
-    private String createdBy;
-
-    @LastModifiedDate
-    @Column(nullable = false)
-    private LocalDateTime modifiedAt;
-
-    @LastModifiedBy
-    @Column(nullable = false, length = 100)
-    private String modifiedBy;
 
     private Article(String title, String content, String hashtag) {
         this.title = title;
